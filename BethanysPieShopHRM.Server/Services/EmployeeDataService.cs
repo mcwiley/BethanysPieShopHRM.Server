4ae94848affbb8ef8ce714b4023b7f8e007a2733
Melvin C Wiley <m_c_wiley@hotmail.com>
2019-12-21 08:08:48 -0500

    Updated Solution

---
+++ BethanysPieShopHRM.Server/Services/EmployeeDataService.cs
@@ -0,0 +1,45 @@
+﻿using BethanysPieShopHRM.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM.Server.Services
+{
+    public class EmployeeDataService : IEmployeeDataService
+    {
+        private readonly HttpClient _httpClient;
+
+        public EmployeeDataService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public Task<Employee> AddEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteEmployee(int employeeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task <IEnumerable<Employee>> GetAllEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> GetEmployeeDetails(int employeeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
